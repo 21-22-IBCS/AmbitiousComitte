669df514cf106414b0959839ff6c88ef31cceda4
Thien (Tony) Nguyen <thien_nguyen@m018587.aw.org>
2021-11-16 14:36:51 -0800

    Image Editor Lab

---
+++ Image Editor Lab/ImageEditor.py
@@ -0,0 +1,52 @@
+from graphics import*
+from Button import*
+
+def brighten(cats):
+    return None
+def darken(cats):
+    return None
+def blurr(cats):
+    return None
+def contrast(cats):
+    return None
+def specialFilter(cats):
+    return None
+def main():
+
+    win = GraphWin("Image Editor", 800, 600)
+    sh = Button(win, "white", "Show", Point(150, 40), 45)
+    hi = Button(win, "white", "Hide", Point(300, 40), 45)
+    close = Button(win, "grey", "Quit", Point(150, 560), 45)
+    bright = Button(win, "white", "Brighten", Point(720, 50), 45)
+    dark = Button(win, "white", "Darken", Point(720, 150), 45)
+    blur = Button(win, "white", "Blur", Point(720, 250), 45)
+    cont = Button(win, "white", "Contrast", Point(720, 350), 45)
+    filt = Button(win, "white", "Filter", Point(720, 450), 45)
+
+    cats = Image(Point(400,300), "Cats.png")
+
+    m = win.getMouse()
+    while True:
+        if close.isClicked(m):
+            break
+        if sh.isClicked(m):
+            cats.undraw()
+            cats.draw(win)
+        if hi.isClicked(m):
+            cats.undraw()
+        if dark.isClicked(m):
+            darken(cats)
+        if bright.isClicked(m):
+            brighten(cats)
+        if blur.isClicked(m):
+            blurr(cats)
+        if cont.isClicked(m):
+            contrast(cats)
+        if filt.isClicked(m):
+            specialFilter(cats)
+        m = win.getMouse()
+
+    win.close()
+    
+if __name__ == "__main__":
+    main()
